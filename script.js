d441873111030c6818ee4de6e07507b7ae011ebe
Michael <maicolriola@hotmail.es>
2020-10-20 09:17:04 -0500

    Se realiza un buscardor de videos por medio de la api Youtube Data Api v3

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+    var API_KEY = "AIzaSyBJ0ktJPJjTqjaugr8kA01TqHGKd6roX34"
+
+    var video = ''
+
+
+    $("#form").submit(function(event) {
+        event.preventDefault()
+        
+
+        var search = $("#search").val()
+    
+        videoSearch(API_KEY,search,10)
+    })
+
+    function videoSearch(key,search,maxResults){
+        $("#videos").empty()
+
+        $.get("https://www.googleapis.com/youtube/v3/search?key="+ key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,function(data){
+            console.log(data)
+
+            data.items.forEach(item => {
+                video = `
+                
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+
+                
+                `
+
+                $("#videos").append(video)
+                
+            });
+        })
+
+
+    }
+
+    
+})
